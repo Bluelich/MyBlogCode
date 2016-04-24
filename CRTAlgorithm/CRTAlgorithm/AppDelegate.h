3b72cd3a8c84e3748b5e6509dd34d50ea9d82416
bluelich <bluelich@qq.com>
2016-04-24 18:59:19 +0800

    Firt commit

---
+++ CRTAlgorithm/CRTAlgorithm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CRTAlgorithm
+//
+//  Created by 周强 on 16/4/24.
+//  Copyright © 2016年 Bluelich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CRTAlgorithm/CRTAlgorithm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CRTAlgorithm
+//
+//  Created by 周强 on 16/4/24.
+//  Copyright © 2016年 Bluelich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
